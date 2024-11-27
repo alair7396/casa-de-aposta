b854a99bd130ee43797898c69ddfe8fb5ab4a1db
alair7396 <alair_luiz@estudante.sc.senai.br>
2024-11-27 15:41:16 -0400

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,38 +3,39 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
- plugins: [
-   react(),
-   VitePWA({
-     registerType: 'autoUpdate',
-     includeAssets: [
-       'favicon.svg',
-       'favicon.ico',
-       'robots.txt',
-       'apple-touch-icon.png',
-     ],
-     manifest: {
-      name: 'LuckDuck jogos da sorte',
-      short_name: 'LuckDuck',
-      description: 'É um projeto para jogos da sorte',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
-      display: 'standalone',
-       icons: [
-         {
-           src: '/icons/icon-192x192-transparent.png',
-           sizes: '192x192',
-           type: 'image/png',
-         },
-         {
-           src: '/icons/icon-512x512-transparent.png',
-           sizes: '512x512',
-           type: 'image/png',
-         },
-       ],
-     },
-   }),
- ],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'LuckDuck jogos da sorte',
+        short_name: 'LuckDuck',
+        description: 'É um projeto para jogos da sorte',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon-192x192-transparent.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512-transparent.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist', // Certifique-se de que os arquivos são gerados no diretório dist
+  },
 });
-
-
